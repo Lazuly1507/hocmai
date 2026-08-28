f78f448eee344ba53d1c7be5dd6fa177e2388a07
Lazuly <nhlazuly@gmail.com>
2026-08-28 20:19:11 +0700

    buoi 13

--- data.js
+++ data.js
@@ -13,14 +13,22 @@
 const DATA = {
   "Toán": {
     "Tháng 8": {
-      "Buổi 13": [
+      "Buổi 10": [
         "https://cdn2.hocmai.vn/dwx1bLucmnK0sDhOMojY0Q/1787921502/hocmaivideos2/stream/0a4a85ee889497116f2b43c46012e85c/playlist_480.m3u8",
         "https://cdn2.hocmai.vn/q503rk6obYsth6SddJDxag/1787921502/hocmaivideos2/stream/78eee32d841e510e4ee6f994d6e1ebf3/playlist_720.m3u8",
         "https://cdn2.hocmai.vn/k2bWaSyFie00p0Ex0EhG6A/1787921502/hocmaivideos2/stream/d7a550e11a8294b268d42ca0a644e71b/playlist_720.m3u8",
         "https://cdn2.hocmai.vn/LRoPbvRegqxQ68mM7_6arA/1787921502/hocmaivideos2/stream/1f74cb46f92b463cc43fd4ab3f2050f4/playlist_720.m3u8",
         "https://cdn2.hocmai.vn/F2N1RUD_tA0SL4c-TYNprQ/1787921502/hocmaivideos2/stream/8b1ffc84bd34b050bf17b9cf14b0782f/playlist_720.m3u8",
       ],
-      "Buổi 2": [
+      "Buổi 13": [
+        "https://cdn2.hocmai.vn/atZ588LiewqEcmOvASnPVQ/1787923537/hocmaivideos2/stream/6b584ef2358a5fad9a9a4ca84b8dd808/playlist_480.m3u8",
+        "https://cdn2.hocmai.vn/wLqKvK6TpYtDnhe6CdR77A/1787923537/hocmaivideos2/stream/69278a4f346034025e7078c12b882bae/playlist_720.m3u8",
+        "https://cdn2.hocmai.vn/jeH-1oRx3XDVwr_FnU-5MA/1787923537/hocmaivideos2/stream/e401fa5041d5270fbcdc2293db21d8c0/playlist_720.m3u8",
+        "https://cdn2.hocmai.vn/A6Gae8pWpho8LyKkUDkW7g/1787923537/hocmaivideos2/stream/044ef81bbc41f7d8daec6726ef5ee6b5/playlist_720.m3u8",
+        "https://cdn2.hocmai.vn/2xuTLVH4ywxklDuKp5eItw/1787923537/hocmaivideos2/stream/6715a17ec795094b67ee9e2dcbfe3dc2/playlist_720.m3u8",
+        "https://cdn2.hocmai.vn/pqq9Gl8gFJsh8WYkkixXIw/1787923537/hocmaivideos2/stream/6366cb5f1b98ac52e57b1303eb7128cc/playlist_720.m3u8",
+        "https://cdn2.hocmai.vn/OZd2Y62k6geVEc2Nmq-y4g/1787923537/hocmaivideos2/stream/e821dcdcfd849998826ede325c56117c/playlist_720.m3u8",
+        "https://cdn2.hocmai.vn/QjrXhvNvbSzo-TLWna6BGQ/1787923537/hocmaivideos2/stream/21afa1ed95feb649020513458522996a/playlist_720.m3u8",
       ],
     },
   },
