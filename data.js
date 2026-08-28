f326aa17941163183021ae5659b0c19a22b1eab5
Lazuly <nhlazuly@gmail.com>
2026-08-28 19:06:24 +0700

    buổi 13

--- data.js
+++ data.js
@@ -14,7 +14,11 @@ const DATA = {
   "Toán": {
     "Tháng 8": {
       "Buổi 13": [
-        "https://cdn2.hocmai.vn/mL8GzQPZ_7R5elUUJdxOBw/1787918414/hocmaivideos2/stream/6b584ef2358a5fad9a9a4ca84b8dd808/playlist_480.m3u8",
+        "https://cdn2.hocmai.vn/dwx1bLucmnK0sDhOMojY0Q/1787921502/hocmaivideos2/stream/0a4a85ee889497116f2b43c46012e85c/playlist_480.m3u8",
+        "https://cdn2.hocmai.vn/q503rk6obYsth6SddJDxag/1787921502/hocmaivideos2/stream/78eee32d841e510e4ee6f994d6e1ebf3/playlist_720.m3u8",
+        "https://cdn2.hocmai.vn/k2bWaSyFie00p0Ex0EhG6A/1787921502/hocmaivideos2/stream/d7a550e11a8294b268d42ca0a644e71b/playlist_720.m3u8",
+        "https://cdn2.hocmai.vn/LRoPbvRegqxQ68mM7_6arA/1787921502/hocmaivideos2/stream/1f74cb46f92b463cc43fd4ab3f2050f4/playlist_720.m3u8",
+        "https://cdn2.hocmai.vn/F2N1RUD_tA0SL4c-TYNprQ/1787921502/hocmaivideos2/stream/8b1ffc84bd34b050bf17b9cf14b0782f/playlist_720.m3u8",
       ],
       "Buổi 2": [
       ],
